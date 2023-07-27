f5efd2ec09495a479d2eb7a1718389f1200a3d0e
m1sk9 <me@lilis.dev>
2023-07-27 14:48:07 +0900

    feat: message limit bypass

--- src/api/discord.rs
+++ src/api/discord.rs
@@ -1,7 +1,7 @@
 use serenity::model::prelude::Message;
 use serenity::prelude::Context;
 
-pub async fn send_message(ctx: &Context, msg: &Message, content: &str) -> Message {
+pub async fn reply(ctx: &Context, msg: &Message, content: &str) -> Message {
     msg.reply_ping(ctx, content)
         .await
         .expect("メッセージの送信に失敗しました。")
@@ -12,4 +12,18 @@ pub async fn edit_response(ctx: &Context, mut target_message: Message, response:
         .edit(ctx, |m| m.content(response))
         .await
         .expect("メッセージの編集に失敗しました。");
+}
+
+pub async fn edit_response_with_file(ctx: &Context, mut target_msg: Message, file_name: &str) {
+    target_msg
+        .edit(ctx, |m| {
+            m.content("レスポンスが2000文字を超えたため、添付ファイルで表示します。")
+        })
+        .await
+        .expect("メッセージの編集に失敗しました。");
+    target_msg
+        .channel_id
+        .send_message(ctx, |m| m.add_file(file_name))
+        .await
+        .expect("メッセージの送信に失敗しました。");
 }
--- src/events/message.rs
+++ src/events/message.rs
@@ -2,22 +2,19 @@ use serenity::model::prelude::Message;
 use serenity::prelude::Context;
 
 use crate::api::chatgpt::chat_completion;
-use crate::api::discord::{edit_response, send_message};
+use crate::api::discord::{edit_response, edit_response_with_file, reply};
+use crate::utils::create_temp_file;
 
 pub async fn chat_ai(ctx: &Context, msg: Message) {
-    let waiting_message = send_message(ctx, &msg, "思考中... 🤔").await;
+    let waiting_message = reply(ctx, &msg, "思考中... 🤔").await;
 
     let response = chat_completion(&msg.content).await;
     let response_content = &response.message().content;
 
     match response_content.chars().count() {
         count if count > 2000 => {
-            send_message(
-                ctx,
-                &msg,
-                "レスポンスが2000文字を超えたため、表示できません。",
-            )
-            .await;
+            create_temp_file(response_content.to_string());
+            edit_response_with_file(&ctx, waiting_message, "temp.txt").await;
         }
         _ => edit_response(&ctx, waiting_message, response_content).await,
     }
