69993316060cc2da9e98c80effe547adfbd381df
m1sk9 <me@lilis.dev>
2023-07-30 02:30:42 +0900

    refactor: remove edit_response_with_file

--- src/api/discord.rs
+++ src/api/discord.rs
@@ -13,17 +13,3 @@ pub async fn edit_response(ctx: &Context, mut target_message: Message, response:
         .await
         .expect("メッセージの編集に失敗しました。");
 }
-
-pub async fn edit_response_with_file(ctx: &Context, mut target_msg: Message, file_name: &str) {
-    target_msg
-        .edit(ctx, |m| {
-            m.content("レスポンスが2000文字を超えたため、添付ファイルで表示します。")
-        })
-        .await
-        .expect("メッセージの編集に失敗しました。");
-    target_msg
-        .channel_id
-        .send_message(ctx, |m| m.add_file(file_name))
-        .await
-        .expect("メッセージの送信に失敗しました。");
-}
