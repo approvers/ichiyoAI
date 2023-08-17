5ecec50d50f026ef54d86d81b5279fb45f4e5c70
m1sk9 <me@m1sk9.dev>
2023-08-18 04:05:18 +0900

    feat: insert system_context

--- src/service/chat.rs
+++ src/service/chat.rs
@@ -1,31 +1,31 @@
+use anyhow::Ok;
 use chatgpt::prelude::ChatGPTEngine;
-use chatgpt::types::CompletionResponse;
 use once_cell::sync::OnceCell;
 use serenity::model::prelude::Message;
 use serenity::prelude::Context;
 
 use crate::client::openai::request_message;
+use crate::model::{ReplyMessage, ReplyRole};
 
 pub async fn chat_mode(ctx: &Context, msg: &Message, model: ChatGPTEngine) -> anyhow::Result<()> {
-    let response = get_response(ctx, msg, model).await?;
-    let reply_content = &response.message().content;
+    let reply = get_reply(ctx, msg).await?;
+    let reply_message = request_message(&reply, model).await?;
 
-    msg.reply_ping(ctx, reply_content).await?;
+    msg.reply_ping(ctx, reply_message).await?;
 
     Ok(())
 }
 
-async fn get_response(
-    ctx: &Context,
-    msg: &Message,
-    model: ChatGPTEngine,
-) -> anyhow::Result<CompletionResponse> {
+async fn get_reply(ctx: &Context, msg: &Message) -> anyhow::Result<Vec<ReplyMessage>> {
     static OWN_MENTION: OnceCell<String> = OnceCell::new();
     let mention = OWN_MENTION.get_or_init(|| format!("<@{}>", ctx.cache.current_user_id()));
-
     let content = msg.content.replace(mention, "").trim().to_string();
 
-    let response = request_message(content, model).await?;
+    let mut replies: Vec<ReplyMessage> = vec![ReplyMessage {
+        role: ReplyRole::User,
+        content,
+    }];
 
-    Ok(response)
+    replies.reverse();
+    Ok(replies)
 }
