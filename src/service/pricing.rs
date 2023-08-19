22f6e36398f548e323bccac5bd0861123d51bb45
watano1168 <watano1168@gmail.com>
2023-08-20 05:04:06 +0900

    値段計算処理を改善

--- src/service/pricing.rs
+++ src/service/pricing.rs
@@ -1,30 +1,28 @@
 use chatgpt::prelude::ChatGPTEngine;
 
-const GPT3_5_PRICE_PER_INPUT_1K_TOKEN_USD: f32 = 0.0015;
-const GPT3_5_PRICE_PER_OUTPUT_1K_TOKEN_USD: f32 = 0.002;
-const GPT4_PRICE_PER_INPUT_1K_TOKEN_USD: f32 = 0.03;
-const GPT4_PRICE_PER_OUTPUT_1K_TOKEN_USD: f32 = 0.06;
+// 桁落ちを防ぐため、10,000,000倍して計算する
+const SCALE: f32 = 10_000_000.0;
 
 // クレジットカード会社の手数料も含めて、多めに設定
-const EXCHANGE_RATE_USD_PER_JPY: f32 = 150.0;
+const EXCHANGE_RATE: f32 = 150.0;
+
+// https://openai.com/pricing
+const GPT3_5_JPY_PER_INPUT_TOKEN: u32 = (0.0015 * EXCHANGE_RATE * SCALE / 1000.0) as u32;
+const GPT3_5_JPY_PER_OUTPUT_TOKEN: u32 = (0.002 * EXCHANGE_RATE * SCALE / 1000.0) as u32;
+const GPT4_JPY_PER_INPUT_TOKEN: u32 = (0.03 * EXCHANGE_RATE * SCALE / 1000.0) as u32;
+const GPT4_JPY_PER_OUTPUT_TOKEN: u32 = (0.06 * EXCHANGE_RATE * SCALE / 1000.0) as u32;
 
 pub fn usage_pricing(input_token: u32, output_token: u32, model: ChatGPTEngine) -> f32 {
-    let input_token_per_usd = match model {
-        ChatGPTEngine::Gpt35Turbo => GPT3_5_PRICE_PER_INPUT_1K_TOKEN_USD,
-        ChatGPTEngine::Gpt4 => GPT4_PRICE_PER_INPUT_1K_TOKEN_USD,
+    let input_rate = match model {
+        ChatGPTEngine::Gpt35Turbo => GPT3_5_JPY_PER_INPUT_TOKEN,
+        ChatGPTEngine::Gpt4 => GPT4_JPY_PER_INPUT_TOKEN,
         _ => panic!("Invalid model: {:?}", model),
-    } * 0.001;
-    let output_token_per_usd = match model {
-        ChatGPTEngine::Gpt35Turbo => GPT3_5_PRICE_PER_OUTPUT_1K_TOKEN_USD,
-        ChatGPTEngine::Gpt4 => GPT4_PRICE_PER_OUTPUT_1K_TOKEN_USD,
+    };
+    let output_rate = match model {
+        ChatGPTEngine::Gpt35Turbo => GPT3_5_JPY_PER_OUTPUT_TOKEN,
+        ChatGPTEngine::Gpt4 => GPT4_JPY_PER_OUTPUT_TOKEN,
         _ => panic!("Invalid model: {:?}", model),
-    } * 0.001;
+    };
 
-    let input_usd = input_token as f32 * input_token_per_usd;
-    let output_usd = output_token as f32 * output_token_per_usd;
-
-    let usd = input_usd + output_usd;
-    let jpy = usd * EXCHANGE_RATE_USD_PER_JPY;
-
-    jpy
+    (input_rate * input_token + output_rate * output_token) as f32 / SCALE
 }
