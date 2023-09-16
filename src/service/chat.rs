19136a4a434c05d337c541b465926f54b9924d2d
Sho Sakuma <me@lilis.dev>
2023-09-16 13:05:51 +0900

    feat: 応答メッセージに合計トークン数を表示するように (#102)

--- src/service/chat.rs
+++ src/service/chat.rs
@@ -34,9 +34,10 @@ async fn get_reply(ctx: &Context, msg: &Message) -> anyhow::Result<Vec<ReplyMess
 fn format_result(result: MessageCompletionResult, model: ChatGPTEngine) -> String {
     let pricing = usage_pricing(result.input_token, result.output_token, model);
     format!(
-        "{}\n\n`利用料金: ￥{:.2}` - `使用モデル: {}`",
+        "{}\n\n`利用料金: ￥{:.2}(合計トークン合計: {})` - `使用モデル: {}`",
         result.message,
         pricing,
+        result.total_token,
         model.to_string()
     )
 }
