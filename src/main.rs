a8bfa76ee9ccb8717fa5e741fe898d6b9f906235
Sho Sakuma <me@m1sk9.dev>
2023-09-23 20:15:09 +0900

    refactor: sentry の廃止 (#108)

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,6 @@ async fn main() -> anyhow::Result<()> {
     load_env();
     tracing_subscriber::fmt().compact().init();
 
-    let _guard = if cfg!(feature = "enable_sentry") {
-        let _guard = sentry::init((
-            "https://f3fd3cb315dd947285acd406f656e0f9@o4505761355988992.ingest.sentry.io/4505761369030656", sentry::ClientOptions {
-                release: sentry::release_name!(),
-                ..Default::default()
-            }
-        ));
-        Some(_guard)
-    } else {
-        None
-    };
-
     let token = get_env("DISCORD_API_TOKEN");
 
     start_discord_client(token.as_str()).await?;
