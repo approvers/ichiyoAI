a72bd3ea88a24f743c59520c4493d8f41fa7837f
m1sk9 <me@m1sk9.dev>
2023-11-07 22:27:58 +0900

    feat: toggle log level

--- src/model/env.rs
+++ src/model/env.rs
@@ -1,5 +1,21 @@
 use once_cell::sync::OnceCell;
 use serde::{Deserialize, Serialize};
+use strum::{Display, EnumString};
+
+#[derive(Serialize, Deserialize, Debug, EnumString, Display)]
+pub enum LogEnvironment {
+    #[strum(serialize = "production")]
+    Production,
+
+    #[strum(serialize = "development")]
+    Development,
+}
+
+impl Default for LogEnvironment {
+    fn default() -> Self {
+        LogEnvironment::Production
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct IchiyoAiEnv {
@@ -7,6 +23,8 @@ pub struct IchiyoAiEnv {
     pub openai_api_key: String,
     pub guild_id: u64,
     pub taxpayer_role_id: u64,
+    #[serde(default)]
+    pub log_environment: LogEnvironment,
 }
 
 pub static ICHIYOAI_ENV: OnceCell<IchiyoAiEnv> = OnceCell::new();
