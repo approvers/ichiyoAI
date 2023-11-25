9d4ab0c89e4e9daa7fed4b594761a2a7d8e66baf
m1sk9 <me@m1sk9.dev>
2023-11-25 20:54:23 +0900

    refactor: remove older code

--- src/model/env.rs
+++ src/model/env.rs
@@ -1,30 +1,12 @@
 use once_cell::sync::OnceCell;
 use serde::{Deserialize, Serialize};
-use strum::{Display, EnumString};
-
-#[derive(Serialize, Deserialize, Debug, EnumString, Display)]
-pub enum LogEnvironment {
-    #[strum(serialize = "production")]
-    Production,
-
-    #[strum(serialize = "development")]
-    Development,
-}
-
-impl Default for LogEnvironment {
-    fn default() -> Self {
-        LogEnvironment::Production
-    }
-}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct IchiyoAiEnv {
     pub discord_api_token: String,
     pub openai_api_key: String,
     pub guild_id: u64,
-    pub taxpayer_role_id: u64,
-    #[serde(default)]
-    pub log_environment: LogEnvironment,
+    pub sponsor_role_id: u64,
 }
 
 pub static ICHIYOAI_ENV: OnceCell<IchiyoAiEnv> = OnceCell::new();
