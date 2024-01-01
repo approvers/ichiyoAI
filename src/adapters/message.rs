17f4006b6a84c89f85cc30d6ab390c99743c3df6
Nanai Jua <me@n7i.dev>
2024-01-02 02:51:39 +0900

    feat(bin): rewrite `EvHandler::message` with internal wrapper

--- src/adapters/message.rs
+++ src/adapters/message.rs
@@ -1,92 +1,72 @@
 use std::sync::Arc;
 
 use anyhow::{Context as _, Ok};
-use async_openai::types::{
-    ChatCompletionRequestAssistantMessageArgs, ChatCompletionRequestMessage,
-    ChatCompletionRequestUserMessageArgs, Role,
-};
 use serenity::{
     all::ChannelId,
+    all::MessageId,
     client::Context,
     http::{Http, Typing},
     model::channel::Message,
 };
 
-use crate::{
-    event::OWN_MENTION,
-    model::chatgpt::{usage_pricing, ChatGPTResponseModel},
-};
+use crate::event::OWN_MENTION;
 
 pub fn start_typing(http: Arc<Http>, channel_id: ChannelId) -> Typing {
     Typing::start(http, channel_id)
 }
 
 pub async fn push_referenced_msg_to_prompts(
-    prompts: &mut Vec<ChatCompletionRequestMessage>,
     ctx: &Context,
-    msg: &Message,
+    messages: &mut Vec<ichiyo_ai::Message<()>>,
+    channel_id: ChannelId,
+    referenced_id: MessageId,
 ) -> anyhow::Result<()> {
     let mention = OWN_MENTION.get_or_init(|| format!("<@{}>", ctx.cache.current_user().id));
-    let channel_id = msg.channel_id;
-    let mut target_message_id = msg.referenced_message.as_ref().map(|m| m.id);
 
-    while let Some(message_id) = target_message_id {
-        let ref_msg = ctx
+    let mut next = Some(referenced_id);
+    while let Some(message_id) = next {
+        let msg = ctx
             .http
             .clone()
             .get_message(channel_id, message_id)
             .await
             .context("Failed to get referenced message")?;
 
-        let role = if ref_msg.is_own(ctx) {
-            Role::Assistant
-        } else {
-            Role::User
-        };
+        let mut content = msg.content.replace(mention, "").trim().to_string();
+        let is_own = msg.is_own(ctx);
 
-        let mut content = ref_msg.content.replace(mention, "").trim().to_string();
-
-        match role {
-            Role::Assistant => {
-                let len = content.rfind("\n\n").unwrap_or(content.len());
-                content.truncate(len);
-
-                prompts.push(
-                    ChatCompletionRequestAssistantMessageArgs::default()
-                        .content(content)
-                        .build()?
-                        .into(),
-                )
-            }
-            Role::User => prompts.push(
-                ChatCompletionRequestUserMessageArgs::default()
-                    .content(content)
-                    .build()?
-                    .into(),
-            ),
-            _ => {}
+        if is_own {
+            let len = content.rfind("\n\n").unwrap_or(content.len());
+            content.truncate(len);
         }
 
-        target_message_id = ref_msg.referenced_message.as_ref().map(|m| m.id);
+        messages.push({
+            match is_own {
+                true => ichiyo_ai::Message::Model { id: (), content },
+                false => ichiyo_ai::Message::User { id: (), content },
+            }
+        });
+
+        next = msg.referenced_message.as_ref().map(|m| m.id);
     }
 
     Ok(())
 }
 
 pub async fn reply_chatgpt_response(
-    res: ChatGPTResponseModel,
     ctx: &Context,
     msg: &Message,
+    content: impl core::fmt::Display,
+    metadata: &impl ichiyo_ai::Metadata,
 ) -> anyhow::Result<()> {
-    let usage = usage_pricing(res.prompt_tokens, res.completion_tokens, &res.model);
+    let price_yen = metadata.price_yen();
+    let tokens = metadata.tokens();
+    let model = metadata.by();
+
+    let content = format!("{content}\n\n{model}￤¥{price_yen:.2}￤{tokens} tokens");
+
     let _ = msg
-        .reply_ping(
-            &ctx,
-            format!(
-                "{}\n\n{}￤¥{:.2}￤{} tokens",
-                res.res, res.model, usage, res.total_tokens
-            ),
-        )
+        .reply_ping(&ctx, content)
         .await
         .context("Failed to reply.");
 
