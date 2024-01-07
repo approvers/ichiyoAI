b2719e439e802f9c8fb92c96b4c49a9ad39a28bf
Nanai Jua <me@n7i.dev>
2024-01-08 02:56:48 +0900

    feat(bin): make sentry to be switchable

--- src/bin/main/main.rs
+++ src/bin/main/main.rs
@@ -12,7 +12,7 @@ pub struct IchiyoAiEnv {
     pub google_ai_api_key: String,
     pub guild_id: u64,
     pub sponsor_role_id: u64,
-    pub sentry_dsn: String,
+    pub sentry_dsn: Option<String>,
 }
 
 pub fn envs() -> &'static IchiyoAiEnv {
@@ -26,15 +26,24 @@ async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv().ok();
     let envs = crate::envs();
 
-    // let _guard = sentry::init((
-    //     &*envs.sentry_dsn,
-    //     sentry::ClientOptions {
-    //         release: sentry::release_name!(),
-    //         ..Default::default()
-    //     },
-    // ));
-
     tracing_subscriber::fmt().compact().init();
+
+    // `sentry::ClientInitGuard` must be stored to keep the transport alive.
+    let guard = envs.sentry_dsn.as_ref().map(|dsn| {
+        sentry::init((
+            dsn.as_str(),
+            sentry::ClientOptions {
+                release: sentry::release_name!(),
+                ..Default::default()
+            },
+        ))
+    });
+
+    if guard.is_some() {
+        tracing::info!("Sentry is initialized");
+    } else {
+        tracing::warn!("Sentry isn't initialized");
+    }
 
     let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
     let mut client = serenity::Client::builder(&envs.discord_api_token, intents)
